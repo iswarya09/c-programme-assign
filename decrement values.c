f70040a9d2010083591ea332ea4fc451a024f553
iswarya09 <62959871+iswarya09@users.noreply.github.com>
2020-04-10 23:29:09 +0530

    Add files via upload

---
+++ decrement values.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+    int main() {
+      
+    
+    int n;
+       printf("enter a number:\n");
+       scanf("%d",&n);
+       do {
+         printf("%d\n",n);
+         n--;
+         
+       }
+       
+    
+    while(n>=0);
+    return 0;
+    }
